7a1fe1866d1afdbef9d8476178511de361288a00
Wayan Tisna <wayantisna74@gmail.com>
2025-01-23 08:23:50 +0800

    refactor: remove unused files and components to streamline the project structure

--- nextjs/components/container.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, PropsWithChildren } from "react"
-
-const Container: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="container mx-auto px-5">{children}</div>
-}
-
-export default Container
